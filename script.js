ac54457fb8ce20bd1355810885133b7822cf5d7f
JingYee-JY <jingyeetan2005@gmail.com>
2022-10-20 14:22:35 +0800

    Update

--- script.js
+++ script.js
@@ -14,10 +14,16 @@ const optional =  document.querySelector(".optional")
 const playAgain = document.querySelector(".againButton")
 const final = document.querySelector(".final");
 const currentSong = document.querySelector(".currentSong");
+const homeButton = document.querySelector(".homeButton")
 
 const keys = document.querySelectorAll('.key')
 const whiteKeys = document.querySelectorAll('.key.white')
 const blackKeys = document.querySelectorAll('.key.black')
+
+const clickSound = document.getElementById("click")
+const basicNotes = document.getElementById("basicNotes")
+const littleStar = document.getElementById("littleStar")
+const happyBirthday = document.getElementById("happyBirthday")
 
 let song
 let play
@@ -34,63 +40,164 @@ const starSong = ["C","C","G","G", "A","A", "G"]
 const birthdaySong = ["C","C", "D", "C", "F", "E", "C", "C", "D", "C","G", "F"] 
 
 startButton.addEventListener("click", () => {
-  start.classList.add("hide")
-  selection.classList.remove("hide")
+  playClickSound()
+  let delay = setTimeout(() => {
+    start.classList.add("hide")
+    selection.classList.remove("hide")
+  }, 200);
 })
 
 basic.addEventListener("click", () => {
-  selection.classList.add("hide")
-  game.classList.remove("hide")
+  playClickSound()
   song = basicSong
-  current = 1
   currentSong.innerHTML = `Basic`
   play = computerInput = playerInput = false
-  Question()
+  let delay = setTimeout(() => {
+    Start()
+  }, 200)
 })
 
 star.addEventListener("click", () => {
-  selection.classList.add("hide")
-  game.classList.remove("hide")
+  playClickSound()
   song = starSong
-  current = 1
   currentSong.innerHTML = `Little Star`
-  play = computerInput = playerInput = false
-  Question()
+  let delay = setTimeout(() => {
+    Start()
+  }, 200)
 })
 
 birthday.addEventListener("click", () => {
-  selection.classList.add("hide")
-  game.classList.remove("hide")
+  playClickSound()
   currentSong.innerHTML = `Happy Birthday`
   song = birthdaySong
-  current = 1
-  play = computerInput = playerInput = false
-  Question()
+  let delay = setTimeout(() => {
+    Start()
+  }, 200)
+
 })
 
 playAgain.addEventListener("click", () => {
-  final.classList.add("hide")
-  start.classList.remove("hide")
+  playClickSound()
+  let delay = setTimeout(() => {
+    final.classList.add("hide")
+    start.classList.remove("hide")
+  }, 200)
+})
+
+homeButton.addEventListener("click", () => {
+  playClickSound()
+  let delay = setTimeout(() => {
+      location.assign('https://gimme.sg/activations/dementia/');
+    }, 200);
+})
+
+function Start() {
+  selection.classList.add("hide")
+  game.classList.remove("hide")
+  current = 1
+  play = computerInput = playerInput = false
+  Question()
+}
+
+keys.forEach(key => {
+
+
+
+
+
+  
+  console.log(key)
+  
+  /*
+  
+      try so ur black keys doesn't get added with these event listener because its not used, idk why
+      
+      the issue just now is because you add the ended listener everytime u click on a note, so there's 
+      like probably 10 listeners each time listening for the answer.
+      
+      so with your timer playing the NPC sound, it might bug out and thats why even when u press the wrong answer,
+      it might think thats the right answer. because the NPC sound is still playing while it's still being "ended"
+      
+      - aden                                                    
+   */
+
+  try { 
+    const noteAudio = document.getElementById(key.dataset.note)
+    noteAudio.addEventListener('ended', () => {
+
+      key.classList.remove('active')
+      if(answer == key.dataset.note && playerInput == true && computerInput == false){
+        optional.style.color = "#51777F"
+        optional.innerHTML = `
+        <img src="./img/right.png">
+        <!--<p>Note ${current} Clear!<p>-->
+        <p>That's right!<p>`
+        current += 1
+        computerSong = 0
+        playerInput = computerInput = false
+        console.log(play)
+        Question()
+        return
+      }
+      if(answer != key.dataset.note && playerInput == true && computerInput == false){
+        optional.style.color = "#C85B5B"
+        optional.innerHTML = `
+        <img src="./img/wrong.png">
+        <p>Note ${current} Try again!<p>`
+        computerSong = 0
+        play = playerInput = computerInput = false
+        console.log(play)
+        Question()
+        return
+      }
+      computerInput = false
+      playerInput = false
+      play = true
+    })
+  }
+  catch(e)
+  {
+    
+  }
+
+ 
+  
 })
 
 keys.forEach(key => {
   key.addEventListener('click', () => {
     playerInput = true
-    playNote(key)})
+   
+    
+    
+    playNote(key)}
+  
+      
+  
+  
+  )
 })
 
 document.addEventListener('keydown', e => {
   if (e.repeat) return
   playerInput = true
+  
   const key = e.key
   const whiteKeyIndex = WHITE_KEYS.indexOf(key)
   const blackKeyIndex = BLACK_KEYS.indexOf(key)
 
   if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
   if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+  
+ 
 })
 
 function playNote(key) {
+  
+ 
+
+
+  
   if(play == false){return}
   if(play == true){
     play = false
@@ -98,7 +205,10 @@ function playNote(key) {
     noteAudio.currentTime = 0
     noteAudio.play()
     key.classList.add('active')
-    noteAudio.addEventListener('ended', () => {
+    
+
+    /*noteAudio.addEventListener('ended', () => {
+      
       key.classList.remove('active')
       if(answer == key.dataset.note && playerInput == true && computerInput == false){
         optional.style.color = "#51777F"
@@ -127,13 +237,31 @@ function playNote(key) {
         computerInput = false
         playerInput = false
         play = true
-    })
+    })*/
   }
+}
+
+function playClickSound(){
+  console.log(clickSound)
+  clickSound.currentTime = 0
+  clickSound.play()
 }
 
 function Question(){
   if(current == (song.length + 1)){
     play = false
+    if(song == basicSong){
+      basicNotes.currentTime = 0
+      basicNotes.play()
+    }
+    if(song == starSong){
+      littleStar.currentTime = 0
+      littleStar.play()
+    }
+    if(song == birthdaySong){
+      happyBirthday.currentTime = 0
+      happyBirthday.play()
+    }
     notes.innerHTML = `-`
     optional.innerHTML =""
     final.classList.remove("hide")
@@ -143,11 +271,18 @@ function Question(){
   answer = song[current - 1]
   score.innerHTML = `${current}/${song.length}`
   let question = document.querySelector(`.${song[current - 1]}`)
+  
   let delay = setTimeout(() => {
     optional.innerHTML =""
     computerInput = true
+
     play = true
     notes.innerHTML = `${answer}`
     playNote(question)
   }, 800);
 }
+
+/*prevent double tag zoom*/
+document.addEventListener('dblclick', function(event) {
+event.preventDefault();
+}, { passive: false });
